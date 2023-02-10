146223d00e12519deb8f17239e1445f2e3b40399
Tri Nguyen <nguyeminhtrii@gmail.com>
2023-02-10 17:43:12 +0700

    complete shows

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,26 +1,16 @@
 import './Home.scss'
 import MovieListing from '../MovieListing/MovieListing'
 import { useEffect } from 'react'
-import movieApi from "../../common/apis/movieApi"
-import {APIKey} from "../../common/apis/movieApiKey"
+
 import { useDispatch } from 'react-redux'
-import { addMovies } from '../../features/movies/movieSlice'
+import { fetchAsyncMovies, fetchAsyncShows } from '../../features/movies/movieSlice'
 
 const Home = () => {
-	const movieText = "Harry"
 	const dispatch = useDispatch()
 	useEffect(() => {
-		const fetchMovies = async () => {
-			const response = await movieApi
-				.get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
-				.catch(err => {
-					console.log("Err :", err)
-				});
-			dispatch(addMovies(response.data))
-		}
-
-		fetchMovies()
-	},[])
+		dispatch(fetchAsyncMovies())
+		dispatch(fetchAsyncShows())
+	},[dispatch])
 
 	return (
 		<div>
--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -1,7 +1,26 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import movieApi from "../../common/apis/movieApi"
+import {APIKey} from "../../common/apis/movieApiKey"
+
+
+// đối số đầu tiên là 1 strings để nhận biết action creator (component nào tạo ra và gửi action đi)
+// đối số thứ 2 là payload creator callback function
+// hàm này sẽ chờ fetch data xong mới return data lên reducer
+export const fetchAsyncMovies = createAsyncThunk('movies/fetchAsyncMovies', async ()=> {
+	const movieText = "Harry"
+	const response = await movieApi.get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
+	return addMovies(response.data)
+})
+
+export const fetchAsyncShows = createAsyncThunk('movies/fetchAsyncShows', async ()=> {
+	const seriesText = "Friends"
+	const response = await movieApi.get(`?apiKey=${APIKey}&s=${seriesText}&type=series`)
+	return addMovies(response.data)
+})
 
 const initialState = {
-	movies: [],
+	movies: {},
+	shows: {}
 }
 
 const movieSlice = createSlice({
@@ -11,9 +30,27 @@ const movieSlice = createSlice({
 		addMovies: (state, {payload}) => {
 			state.movies = payload;
 		}
+	},
+	extraReducers: (builder) => {
+		builder.addCase(fetchAsyncMovies.fulfilled, (state, {payload}) => {
+			console.log("Fetched Successfully!")
+			return {...state,movies: payload.payload}
+		});
+		builder.addCase(fetchAsyncShows.fulfilled, (state, {payload}) => {
+			console.log("Fetched Successfully!")
+			return {...state,shows: payload.payload}
+		});
+		builder.addCase(fetchAsyncMovies.pending, () => {
+			console.log("Pending...")
+
+		});
+		builder.addCase(fetchAsyncMovies.rejected, () => {
+			console.log("Rejected!")
+		});
 	}
 })
 
 export const {addMovies} = movieSlice.actions
 export const getAllMovies = (state) => state.movies.movies  // movie thứ nhất là name of Slice, movie thứ 2 là name of properties (cái lưu trong initialState)
+export const getAllShows = (state) => state.movies.shows
 export default movieSlice.reducer
