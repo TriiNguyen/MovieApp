29a05a950fae225f2c58f93fc6a0e6190c2088b6
Tri Nguyen <nguyeminhtrii@gmail.com>
2023-02-10 11:04:23 +0700

    complete call api

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,30 @@
+import './Home.scss'
+import MovieListing from '../MovieListing/MovieListing'
+import { useEffect } from 'react'
+import movieApi from "../../common/apis/movieApi"
+import {APIKey} from "../../common/apis/movieApiKey"
+
 const Home = () => {
-  return (
-	<div>Home</div>
-  )
+
+	useEffect(() => {
+		const movieText = "Harry"
+		const fetchMovies = async () => {
+			const response = await movieApi
+				.get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
+				.catch(err => {
+					console.log("Err :", err)
+				});
+			console.log("The response from API",response)
+		}
+
+		fetchMovies()
+	},[])
+
+	return (
+		<div>
+			<div className="banner-img"></div>
+			<MovieListing/>
+		</div>
+	)
 }
 export default Home
